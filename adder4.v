514906d8c1f127ab674aa1027cf2863c8ec3afca
iamtongliang <iamtongliang@139.com>
2024-09-01 20:14:51 +0800

    Add files via upload

---
+++ adder4.v
@@ -0,0 +1,50 @@
+`timescale 10ns/1ns
+
+module fulladder_4( X,Y,C,Sum,clk1); 
+    input[3:0] X,Y;
+    output  [3:0] Sum;
+    output  C;
+	input clk1;
+
+    reg [3:0] Cin;
+
+    halfadder_1 h1(
+        .X(X[0]),
+        .Y(Y[0]),
+        .C(Cin[0]),
+        .Sum(Sum[0]),
+        .clk1(clk1)
+        );
+
+
+    fulladder_1 f1(
+        .X(X[1]),
+        .Y(Y[1]),
+        .Cin(Cin[0]),
+        .C(Cin[1]),
+        .Sum(Sum[1]),
+        .clk1(clk1)
+        );
+
+        
+    fulladder_1 f2(
+        .X(X[2]),
+        .Y(Y[2]),
+        .Cin(Cin[1]),
+        .C(Cin[2]),
+        .Sum(Sum[2]),
+        .clk1(clk1)
+        );
+        
+    fulladder_1 f3(
+        .X(X[3]),
+        .Y(Y[3]),
+        .Cin(Cin[2]),
+        .C(Cin[3]),
+        .Sum(Sum[3]),
+        .clk1(clk1)
+        );
+    
+    assign C = Cin[3];
+
+endmodule
---
+++ adderf1.v
@@ -0,0 +1,25 @@
+`timescale 10ns/1ns
+
+module fulladder_1 (
+    input  X,
+	input  Y,
+	input Cin,
+	output   C,
+    output   Sum,
+    input clk1);
+    
+    reg [1:0] buff;
+    reg Xreg, Yreg, Cinreg;
+    initial begin
+        assign Xreg = X;
+        assign Yreg = Y;
+        assign Cinreg = Cin;
+        end
+    always @ (*) begin
+        buff[0] = (Xreg^Yreg)^Cinreg;
+        buff[1] = (Xreg&Yreg)|Cinreg&(Xreg^Yreg);
+        end
+        
+    assign  Sum = buff[0];
+    assign  C = buff[1];
+endmodule
